6117005e723975ed6f77b472b7b09b4bf40cd562
Deepak Singh <singhdeepak5219@gmail.com>
2023-02-05 18:34:45 +0530

    cake container component

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CakeContainer = () => {
+  return (
+    <div>
+        <h1>Cake Container</h1>
+        <button>Buy Cake</button>
+    </div>
+  )
+}
+
+export default CakeContainer
+
