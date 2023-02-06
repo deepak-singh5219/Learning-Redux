a5f83cdd8c2e3017c398f3a852bdc2b444343670
Deepak Singh <singhdeepak5219@gmail.com>
2023-02-06 19:53:16 +0530

    handling negative numbers

--- src/Redux/cakes/cakeReducer.js
+++ src/Redux/cakes/cakeReducer.js
@@ -8,7 +8,7 @@ const cakeReducer = (state = initialState, action) => {
       switch(action.type){
         case BUY_CAKE: return{
              ...state,
-             numOfCakes:state.numOfCakes - 1
+             numOfCakes:(state.numOfCakes - 1>=0)?state.numOfCakes-1:state.numOfCakes
         }
         default: return state;
       }
