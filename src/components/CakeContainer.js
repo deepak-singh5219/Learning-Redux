6a2c99b0e773473a80da9c2db525ac97a04a0ed5
Deepak Singh <singhdeepak5219@gmail.com>
2023-02-06 19:53:51 +0530

    connecting to redux

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux'; 
+import { buyCake } from '../Redux';
+import toast from 'react-hot-toast';
 
-const CakeContainer = () => {
+
+
+const CakeContainer = (props) => {
+  const {numOfCakes,buyCake} = props;
+  
+  const CakeBuy = () => {
+    if(numOfCakes===0) {
+      toast.error('Cakes Finished!!!');
+      return;
+    } 
+
+    buyCake();
+    toast.success("Cake Buy Successfull!!!");
+  }
   return (
     <div>
-        <h1>Cake Container</h1>
-        <button>Buy Cake</button>
+        <h1>Cake Container: {numOfCakes}</h1>
+        <button onClick={CakeBuy}>Buy Cake</button>
     </div>
   )
 }
 
-export default CakeContainer
+const mapStateToProps = (state) => {
+      return {
+        numOfCakes:state.numOfCakes
+      }
+}
+
+const mapDispatchToProps = (dispatch) => {
+      return {
+        buyCake: () => dispatch(buyCake())
+      }
+
+}
+export default connect(mapStateToProps,mapDispatchToProps)(CakeContainer);
 
